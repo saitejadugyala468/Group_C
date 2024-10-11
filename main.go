6d83d1b658e7122d709d824d16d4a41726745498
Shashipreethamgajji <shashipreethamgajji@gmail.com>
2024-10-10 20:17:08 -0400

    Shashi updated

--- main.go
+++ main.go
@@ -1,18 +1,13 @@
 package main
 
 import (
+	"encoding/json"
+	// "log"
 	"net/http"
 	"strconv"
 
 	"github.com/gorilla/mux"
 )
-
-//"encoding/json"
-//"log"
-//"net/http"
-//"strconv"
-
-//"github.com/gorilla/mux"
 
 // Task represents a to-do item with an ID, Title, Description, and Status
 type Task struct {
@@ -24,6 +19,33 @@ type Task struct {
 
 var tasks []Task // In-memory task storage
 var nextID = 1   // Task ID counter
+
+// UpdateTask updates an existing task
+func updateTask(w http.ResponseWriter, r *http.Request) {
+	params := mux.Vars(r)
+	id, err := strconv.Atoi(params["id"])
+	if err != nil {
+		http.Error(w, "Invalid task ID", http.StatusBadRequest)
+		return
+	}
+
+	var updatedTask Task
+	if err := json.NewDecoder(r.Body).Decode(&updatedTask); err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	for i, task := range tasks {
+		if task.ID == id {
+			tasks[i].Title = updatedTask.Title
+			tasks[i].Description = updatedTask.Description
+			tasks[i].Status = updatedTask.Status
+			json.NewEncoder(w).Encode(tasks[i])
+			return
+		}
+	}
+	http.Error(w, "Task not found", http.StatusNotFound)
+}
 
 // DeleteTask removes a task by its ID
 func deleteTask(w http.ResponseWriter, r *http.Request) {
